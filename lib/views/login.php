4b6c2b65401c50ad47912d9d9316c8704d311580
Ted Pak <powerpak006@gmail.com>
2013-07-02 21:53:49 -0400

    you know what we don't need the remember me box

--- lib/views/login.php
+++ lib/views/login.php
@@ -16,14 +16,6 @@
           </select>
         </div>
       </div>
-      <div class="control-group">
-        <div class="controls">
-          <label class="checkbox">
-            <input type="checkbox"> Remember me
-          </label>
-          <button type="submit" class="btn">Sign in</button>
-        </div>
-      </div>
     </form>
 
   </div>
