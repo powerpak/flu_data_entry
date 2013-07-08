f7d5a724212074cdf53532f990d1b784a90fe6f4
Ted Pak <powerpak006@gmail.com>
2013-07-08 16:57:32 -0400

    more tags

--- lib/views/partial/typeahead_data.php
+++ lib/views/partial/typeahead_data.php
@@ -5,7 +5,11 @@ $ANIMAL_SUBTYPES = array(
 );
 $CLINICAL_TAGS = array(
   'weight loss','severe weight loss','no weight loss','nonlethal','lethal','minimal lung pathology',
+  'severe lung pathology','moderate lung pathology','moderate morbidity','moderate bronchitis',
+  'moderate bronchiolitis','moderate inflammation','moderately pathogenic','moderate alveolitis',
   'minimal weight loss','minimal morbidity','severe morbidity','necrotizing bronchitis','bronchitis',
   'alveolitis','inflammation','ruffled fur','hunched posture','severe alveolitis','minimal alveolitis',
-  'severe bronchitis','minimal bronchitis','nonpathogenic'
+  'severe bronchitis','minimal bronchitis','nonpathogenic','bronchiolitis','severe bronchiolitis',
+  'minimal bronchiolitis','necrosis of respiratory epithelium','severe necrosis of respiratory epithelium',
+  'severe inflammation','minimal inflammation'
 );
